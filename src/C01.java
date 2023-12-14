20f49bf066aaafcf2da405a9b063188f8abf2ce2
Nazime <kirkgoznazime@gmail.com>
2023-12-14 09:42:29 +0100

    first commit

---
+++ src/C01.java
@@ -0,0 +1,13 @@
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("gitHub");
+        /*
+        git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nazimekirkgoz/GitHubDeneme.git
+git push -u origin main
+         */
+    }
+}
